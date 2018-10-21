172809fd09788b9ffb39b480fc5b6d9c70ede9e1
Lyttia Cancinos <lyttiacapri@gmail.com>
2018-10-21 14:17:43 -0700

    added predict function and confusion matrix

--- scripts/CART_model.R
+++ scripts/CART_model.R
@@ -15,6 +15,18 @@ library(rpart)
 install.packages("rpart.plot")
 library(rpart.plot)
 
-Crimes4Tree = rpart(violent_crimes ~ month + hour + zipcode + median_value + total_listed, data = Train, method ="class", control = rpart.control(minsplit = 2, minbucket = 2, cp = 0.00002))
+Crimes4Tree = rpart(violent_crimes ~ month + hour + median_value + total_listed, data = Train, method ="class", control = rpart.control(minbucket = 25))
 prp(Crimes4Tree)
 
+PredictCART = predict(Crimes4Tree, newdata = Test, type = "class")
+# this uses a threshold of 0.5 which would be like a baseline model, majority outcome
+
+# create confusion matrix for validation
+table(Test$violent_crimes, PredictCART)
+# PredictCART
+#     0     1
+# 0 43907     0
+# 1  7038     0
+
+(43907+0)/(43907+0+7038+0)
+# 0.861851
