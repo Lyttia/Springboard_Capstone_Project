9701968c9be61d414b0e70a08d07917534553959
Lyttia Cancinos <lyttiacapri@gmail.com>
2018-10-21 15:24:54 -0700

    add evaluation methods to CART script

--- scripts/CART_model.R
+++ scripts/CART_model.R
@@ -21,7 +21,7 @@ prp(Crimes4Tree)
 PredictCART = predict(Crimes4Tree, newdata = Test, type = "class")
 # this uses a threshold of 0.5 which would be like a baseline model, majority outcome
 
-# create confusion matrix for validation
+# create confusion matrix for validation, predicting accuracy at threshold of 0.5
 table(Test$violent_crimes, PredictCART)
 # PredictCART
 #     0     1
@@ -30,3 +30,31 @@ table(Test$violent_crimes, PredictCART)
 
 (43907+0)/(43907+0+7038+0)
 # 0.861851
+
+# Make ROC Curve
+library(ROCR)
+PredictROC = predict(Crimes4Tree, newdata = Test)
+PredictROC
+#             0         1
+#[1,] 0.8618491 0.1381509
+#[2,] 0.8618491 0.1381509
+#[3,] 0.8618491 0.1381509
+#[4,] 0.8618491 0.1381509
+#[5,] 0.8618491 0.1381509
+#[6,] 0.8618491 0.1381509
+#[7,] 0.8618491 0.1381509
+#[8,] 0.8618491 0.1381509
+#[9,] 0.8618491 0.1381509
+#[10,] 0.8618491 0.1381509
+
+# the output is two columns for each test set observation (0 or 1) 
+# the first column is labeled 0 this is the percentage of training 
+# set data in the same subset as that testing observation that had 
+# tested obs output 0  
+# second column is the probability that 
+# use second column when thresholding
+
+pred = prediction(PredictROC[,2], Test$violent_crimes)
+perf = performance(pred, "tpr", "fpr")
+plot(perf)
+# plots ROC curve
