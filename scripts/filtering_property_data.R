91d4df3bfc29e2d98750209631cd143a54aef8b2
Lyttia Cancinos <lyttiacapri@gmail.com>
2018-10-23 13:10:53 -0700

    added intro, data variable, and env clean up

--- scripts/filtering_property_data.R
+++ scripts/filtering_property_data.R
@@ -1,5 +1,11 @@
 # Property Data Cleaning Script
 
+# It is not necessary to use this script if you will 
+# be using the property1.csv file
+# available in the data_files folder of this repo. 
+# This script should be used if you will be obtaining
+# the raw data csv file from the realtor.com website. 
+
 # 0-Install & Load packages=================================================
 
 install.packages("tidyverse", dependencies = TRUE)
@@ -14,8 +20,9 @@ library(devtools)
 # access the original csv file from [Realtor.com]
 # (https://www.realtor.com/research/data)
 
-property_original <- read_csv("RDC_InventoryCoreMetrics_Zip_Hist2.csv")
-property0 <- tbl_df(property_original)
+property_original <- "RDC_InventoryCoreMetrics_Zip_Hist2.csv"
+property.5 <- read_csv(property_original)
+property0 <- tbl_df(property.5)
 
 # 2-Clean Variables/Column Names/Observations===============================
 
@@ -26,10 +33,11 @@ property1 <- property0 %>%
   separate("Month", c("year", "month", "day"), sep = "-") %>% 
   select(-"day")
 
-View(property1)
-
 write_csv(property1, "property1.csv")
 
+# 3- Remove Variables from working memory===================================
+rm(list = ls(pattern = "^property"))
+gc()
 
 
 
